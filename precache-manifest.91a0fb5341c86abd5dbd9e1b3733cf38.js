1f0c52f81a330ae67c7b92daa524565c982fb705
Tarazaaan <omarabousamaha99@gmail.com>
2019-04-26 15:51:23 +0200

    Updates

---
+++ precache-manifest.91a0fb5341c86abd5dbd9e1b3733cf38.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b58c5b7d35f592b99c3",
+    "url": "/GID/static/js/runtime~main.53c23a83.js"
+  },
+  {
+    "revision": "1437df70f460809887af",
+    "url": "/GID/static/js/main.a667ed33.chunk.js"
+  },
+  {
+    "revision": "fc2115ab0c0f9b343512",
+    "url": "/GID/static/js/2.49d43eb3.chunk.js"
+  },
+  {
+    "revision": "1437df70f460809887af",
+    "url": "/GID/static/css/main.ba3131c9.chunk.css"
+  },
+  {
+    "revision": "8a4782bc2cfcfbfb901811ca9ee5fe3a",
+    "url": "/GID/index.html"
+  }
+];
